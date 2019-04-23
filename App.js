e3f8bc945829d03829027cb8e2a58556b38e6fb7
Shukti Shaikh <shukti.azad@gmail.com>
2019-04-23 15:58:02 -0400

    sucessfully getting sensorData

--- App.js
+++ App.js
@@ -1,9 +1,17 @@
 import React, { Component } from 'react';
-import { Text, View, NativeModules, Button } from 'react-native';
+import { Text, View, NativeModules, Button, ul } from 'react-native';
 
 const RNBoseArManager = NativeModules.RNBoseArManager
 
 export default class App extends Component {
+state = {
+    pitch: "pitch",
+    roll: "roll",
+    yaw: "yaw",
+    x: "x",
+    y: "y",
+    z: "z"
+  }
 
 	UNSAFE_componentWillMount() {
       try {
@@ -13,33 +21,50 @@ export default class App extends Component {
       }
   }
 
-  searchForDevice = () => {
+  onSearchForDevice = () => {
     RNBoseArManager.searchForDevice()
       .then(res => console.log(res))
       .catch(e => console.log(e))
   }
 
-  getSensorData = () => {
+  onGetSensorData = () => {
     RNBoseArManager.getSensorData(result => {
-      console.log(result)
-      //pitch = result["pitch"]
+       const { pitch, roll, yaw, x, y, z } = this.state; 
+      this.setState({
+        pitch: result["pitch"],
+        roll: result["roll"],
+        yaw: result["yaw"],
+        x: result["x"],
+        y: result["y"],
+        z: result["z"]
+      });
     })
   }
 
 
   render() {
+    const { pitch, roll, yaw, x, y, z } = this.state; 
+
+
     return (
       <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      
         <Button
-            onPress={this.searchForDevice}
+            onPress={this.onSearchForDevice}
             title="SEARCH FOR DEVICE"
             color="#841584"
         />
         <Button
-            onPress={this.getSensorData}
+            onPress={this.onGetSensorData}
             title="Get Sensor Data"
             color="#841584"
         />
+        <Text>Pitch: {pitch}</Text>
+        <Text>Roll: {roll}</Text>
+        <Text>Yaw: {yaw}</Text>
+        <Text>X: {x}</Text>
+        <Text>Y: {y}</Text>
+        <Text>Z: {z}</Text>
       </View>
     );
   }
