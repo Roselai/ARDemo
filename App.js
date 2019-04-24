bf8963d2fd10dcfba1c68131d04a6a5aad4562b9
Shukti Shaikh <shukti.azad@gmail.com>
2019-04-23 21:12:26 -0400

    added state

--- App.js
+++ App.js
@@ -5,26 +5,40 @@ const RNBoseArManager = NativeModules.RNBoseArManager
 
 export default class App extends Component {
 state = {
-    pitch: "pitch",
-    roll: "roll",
-    yaw: "yaw",
-    x: "x",
-    y: "y",
-    z: "z"
+    isConfigured: false,
+    pitch: "",
+    roll: "",
+    yaw: "",
+    x: "",
+    y: "",
+    z: ""
   }
 
 	UNSAFE_componentWillMount() {
-      try {
+      /*try {
         RNBoseArManager.configure()
       } catch (e) {
         console.error(e.message)
-      }
+      }*/
+      this.configure()
+  }
+
+  configure = () => {
+    RNBoseArManager.configure()
+
+      this.setState({
+        isConfigured: true
+      });
   }
 
   onSearchForDevice = () => {
-    RNBoseArManager.searchForDevice()
-      .then(res => console.log(res))
-      .catch(e => console.log(e))
+    if (this.state.isConfigured) {
+      RNBoseArManager.searchForDevice()
+        .then(res => console.log(res))
+        .catch(e => console.log(e))
+      } else {
+        this.configure()
+      }
   }
 
   onGetSensorData = () => {
@@ -56,15 +70,17 @@ state = {
         />
         <Button
             onPress={this.onGetSensorData}
-            title="Get Sensor Data"
+            title="GET SENSOR DATA"
             color="#841584"
         />
-        <Text>Pitch: {pitch}</Text>
-        <Text>Roll: {roll}</Text>
-        <Text>Yaw: {yaw}</Text>
-        <Text>X: {x}</Text>
-        <Text>Y: {y}</Text>
-        <Text>Z: {z}</Text>
+        <View style={{justifyContent: "center", alignItems: "flex-start" }}>
+          <Text>Pitch: {pitch}</Text>
+          <Text>Roll:  {roll}</Text>
+          <Text>Yaw:   {yaw}</Text>
+          <Text>X:     {x}</Text>
+          <Text>Y:     {y}</Text>
+          <Text>Z:     {z}</Text>
+        </View>
       </View>
     );
   }
